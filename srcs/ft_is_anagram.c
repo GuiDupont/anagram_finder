e28a98676767c2ffd96c8659f07c1f567fb480dd
Guillaume DUPONT <gdupont@student.42.fr>
2020-09-08 13:48:45 +0200

    add makefile and ft_is_anagram

---
+++ srcs/ft_is_anagram.c
@@ -0,0 +1,36 @@
+#include "../libft/libft.h"
+
+int ft_place_is_free(int nb, char *array)
+{
+	if (array[nb] == 0)
+		return (1);
+	return (0);
+}
+
+int		ft_is_anagram(char *word, char *candidate)
+{
+	int		i;
+	int		y;
+	char	taken[20];
+
+	i = 0;
+	ft_memset(taken, 0, 20);
+	while (word[i])
+	{
+		y = 0;
+		while (candidate[y])
+		{
+			if (word[i] == candidate[y] && ft_place_is_free(y, &taken[0]))
+			{
+				taken[y] = 1;
+				break;
+			}
+			y++;
+		}
+		if (!candidate[y])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
